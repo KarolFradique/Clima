6a4d8d5acb59584a25ee907546843ebbc6a7ae39
KarolFradique <karol.fradique@gmail.com>
2021-08-01 12:05:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
+    event.preventDefault()
+
+    let input = document.querySelector('#searchInput').value
+    
+    if(input !=''){
+        clearinfo()
+        showWarning('Carregando...')
+
+        let url =`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=03678479ea257be3f20779bdddabd140&units=metric&lang=pt_br`
+        
+        let results =  await fetch(url)
+        let json = await results.json()
+
+        if(json.cod === 200){
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg,
+            })
+        }else {
+            clearinfo()
+            showWarning('Nao encontramos esta localização')
+        }
+
+    }else{
+        clearinfo()
+    }
+})
+
+function showInfo(json){
+    showWarning('')
+
+    document.querySelector('.resultado').style.display = 'block';
+
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
+    document.querySelector('.tempInfo').innerHTML = `${json.temp}<sup>Cº</sup>` 
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed}<span>km/h</span>`
+
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+    document.querySelector('.ventoPonto').style.transform =`rotate(${json.windAngle - 90}deg)`
+}
+
+function clearinfo(){
+    showWarning('')  
+    document.querySelector('.resultado').style.display = 'none'
+}
+
+function showWarning(msg){
+    document.querySelector('.aviso').innerHTML = msg
+}
